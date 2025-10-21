90323644fa90e81611e4eda39cfab3c182b826b2
13vishank <vasusaini2004@gmail.com>
2025-10-21 22:12:32 +0530

    commit by master_1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -10,6 +10,12 @@ def home():
 def api():
     data = {"message": "Hello from the new and improved API!"}
     return jsonify(data)
+
+@app.route('/todo')
+def todo():
+    return render_template('todo.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
