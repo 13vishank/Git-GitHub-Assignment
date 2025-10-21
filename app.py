2d9b5a96a225a488b183740b2160043df64b5be2
13vishank <vasusaini2004@gmail.com>
2025-10-21 21:37:32 +0530

    Add initial Flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/api')
+def api():
+    data = {"message": "This is the API response"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
