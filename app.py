b4f17c709f04699a83466dae3d8c55d854b388a2
13vishank <vasusaini2004@gmail.com>
2025-10-21 23:13:15 +0530

    Updated To-Do form with Item ID, UUID, and Hash fields (single commit)

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['todo_db']
+collection = db['items']
 
 app = Flask(__name__)
 
@@ -11,11 +16,14 @@ def api():
     data = {"message": "Hello from the new and improved API!"}
     return jsonify(data)
 
-@app.route('/todo')
-def todo():
-    return render_template('todo.html')
-
-
+@app.route('/submittodoitem', methods=['POST'])
+def submittodoitem():
+    item_name = request.form.get('itemName')
+    item_description = request.form.get('itemDescription')
+    if item_name and item_description:
+        collection.insert_one({"itemName": item_name, "itemDescription": item_description})
+        return "Item saved successfully"
+    return "Missing data", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
